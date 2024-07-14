8bb21b4dbe1a06c890398ee714a6f06b102cab7f
Andres Diago <andresdiag@unicauca.edu.co>
2024-07-14 17:21:36 -0500

    :zap: Bcrypt sync and async hashing

--- server.js
+++ server.js
@@ -1,55 +1,35 @@
-'use strict';
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const app         = express();
+"use strict";
+const express = require("express");
+const bodyParser = require("body-parser");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const app = express();
+const bcrypt = require("bcrypt");
 fccTesting(app);
 const saltRounds = 12;
-const myPlaintextPassword = 'sUperpassw0rd!';
-const someOtherPlaintextPassword = 'pass123';
-
+const myPlaintextPassword = "sUperpassw0rd!";
+const someOtherPlaintextPassword = "pass123";
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
-
+let hashDb = "";
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+	/*Store hash in your db*/
+	hashDb = hash;
+	console.log(hash);
+	bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+		/*res == true or false*/
+		console.log(res);
+	});
+});
 //END_ASYNC
 
 //START_SYNC
-
-
-
+console.log("Sync");
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log("Sync: ", result);
 //END_SYNC
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log("Listening on port:", PORT)
+	console.log("Listening on port:", PORT);
 });
